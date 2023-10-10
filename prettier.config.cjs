d4c82c6bdfae8d2186706b9110398ffba1736431
exsjabe <jakob.bergstrom@explizit.se>
2023-10-10 16:27:43 +0200

    initial

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+// @ts-check
+
+/** @type{import('prettier').Config} */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: false,
+  singleQuote: true,
+}
+
+module.exports = config
